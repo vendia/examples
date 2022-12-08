2a784733df60e74f368c6fc2aa9737b13809e48a
David McElligott <david.mcelligott@vendia.net>
2022-12-07 22:27:33 -0600

    feat: Updated login to work with MFA

--- features/share/access-controls/share-auth/cognitoLogin.js
+++ features/share/access-controls/share-auth/cognitoLogin.js
@@ -1,46 +1,85 @@
-import AmazonCognitoIdentity from 'amazon-cognito-identity-js';
-import AWS from 'aws-sdk';
+import AmazonCognitoIdentity from "amazon-cognito-identity-js";
+import AWS from "aws-sdk";
+import inquirer from "inquirer";
 
-export function Login(username, password, userPoolId, clientId, region, identityPoolId) {
+export function Login(
+  username,
+  password,
+  userPoolId,
+  clientId,
+  region,
+  identityPoolId
+) {
+   
+  return new Promise((resolve, reject) => {
+    let authenticationData = { Username: username, Password: password };
+    let authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(
+      authenticationData
+    );
+    let poolData = { UserPoolId: userPoolId, ClientId: clientId };
+    let userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+    let userData = { Username: username, Pool: userPool };
+    let cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+    const onSuccess = (result) => {
+        const identityJwt = result.getIdToken().getJwtToken();
+        let Logins = {};
+        Logins[
+          `cognito-idp.${region}.amazonaws.com/${userPoolId}`
+        ] = identityJwt;
 
-    return new Promise((resolve,reject)=> {
+        AWS.config.region = region;
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials(
+          {
+            IdentityPoolId: identityPoolId,
+            Logins,
+          }
+        );
 
-        let authenticationData = {Username: username, Password: password};
-        let authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
-        let poolData = {UserPoolId: userPoolId, ClientId: clientId};
-        let userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-        let userData = {Username: username, Pool: userPool};
-        let cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+        AWS.config.credentials.get(function () {
+          const accessKeyId = AWS.config.credentials.accessKeyId;
+          const secretAccessKey =
+            AWS.config.credentials.secretAccessKey;
+          const sessionToken = AWS.config.credentials.sessionToken;
 
-        cognitoUser.authenticateUser(authenticationDetails, {
-
-            onSuccess: function(result) {
-                const identityJwt = result.getIdToken().getJwtToken();
-
-                let Logins={};
-                Logins[`cognito-idp.${region}.amazonaws.com/${userPoolId}`] = identityJwt;
-
-                AWS.config.region = region;
-                AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-                    IdentityPoolId: identityPoolId,
-                    Logins
-                });
-
-                AWS.config.credentials.get(
-                    function() {
-                        const accessKeyId = AWS.config.credentials.accessKeyId;
-                        const secretAccessKey = AWS.config.credentials.secretAccessKey;
-                        const sessionToken = AWS.config.credentials.sessionToken;
-
-                        resolve({identityJwt, accessKeyId, secretAccessKey, sessionToken});
-                    }
-                );
-            },
-
-            onFailure: function(err) {
-                reject(err.message || JSON.stringify(err));
-            }
-
+          resolve({
+            identityJwt,
+            accessKeyId,
+            secretAccessKey,
+            sessionToken,
+          });
         });
+      }
+      const onFailure = function (err) {
+          reject(err.message || JSON.stringify(err));
+      };
+    cognitoUser.authenticateUser(authenticationDetails, {
+      totpRequired: function (challengeName, challengeParameters) {
+        inquirer
+          .prompt([
+            {
+              name: "AuthCode",
+              type: "password",
+              message: "Auth code:",
+            },
+          ])
+          .then(
+            (result) => {
+              cognitoUser.sendMFACode(
+                result.AuthCode,
+                {
+                  onSuccess,
+                  onFailure,
+                },
+                challengeName
+              );
+            },
+            (err) => {
+              console.error(err);
+            }
+          );
+      },
+      onSuccess,
+      onFailure,
     });
-};
+  });
+}
